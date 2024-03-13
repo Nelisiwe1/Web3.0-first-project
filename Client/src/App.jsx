2ac0397ac614e7ef437fd623c2e11e45bdfd49c6
Nelisiwe1 <nelisiwengqeme3@gmail.com>
2024-03-14 01:27:09 +0200

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,27 @@
+import {Navbar, Welcome, Footer, Services, Transaction, Loader} from './components'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='min-h-screen'>
+        <div className='gradient-bg-welcome'>
+          <Navbar />
+          <Welcome />
+          </div>
+          <Services />
+          <Transaction />
+          <Footer />
+
+        
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
