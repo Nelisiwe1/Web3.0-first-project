c016a0be3b94222ea7b9af01371b35c5701baa72
Nelisiwe1 <nelisiwengqeme3@gmail.com>
2024-03-18 14:22:02 +0200

    4th

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,8 @@
+//https://eth-sepolia.g.alchemy.com/v2/LLPO90N2szZVVVe3h3n1th8cyXIHcoZ5
+
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
-  solidity: '0.8.0',
-  networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
-    },
-  },
-};
+  Solidity: "0.8.0",
+  networks: {}
+}
